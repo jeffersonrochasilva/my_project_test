2d4ddc9d471600260153f764f1114aaa93bd0f35
Jefferson Rocha <jeffersonrocha@MacBook-Air-de-Jefferson.local>
2025-11-06 16:23:05 -0300

    create navbars

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
 import Header from "../../components/layout/header/index";
+import Navbar from "../../components/layout/navbar";
 const Home = () => {
   return (
     <div className="w-full h-screen px-[50px] py-[20px] bg-[#CFD4E2] flex items-center justify-center">
       <div className="w-full h-full bg-[#fff] rounded-[10px]">
         <Header />
+        <div className="flex w-full h-[calc(100vh-130px)] ">
+          <Navbar />
+        </div>
       </div>
     </div>
   );
